b23cc3cd0224e906b7d003be31d7933d888ebfd4
Nikolay Gorokhov <nikolay.gorokhov@ya.ru>
2023-11-29 16:21:44 +0300

    edit index.html index.css index.js

--- webpack.config.js
+++ webpack.config.js
@@ -18,8 +18,6 @@ module.exports = {
     open: true,
     headers: {
       "Access-Control-Allow-Origin": "*",
-      // "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-      // "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
     }
   },
   module: {
