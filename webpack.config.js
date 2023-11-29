afede5b133abdcfa49a834edfb0678936f9fc32b
Nikolay Gorokhov <nikolay.gorokhov@ya.ru>
2023-11-29 16:12:27 +0300

    init blank

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode:'development',
+  entry: path.resolve(__dirname,'index.js'),
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template:path.resolve(__dirname,'index.html')
+  })],
+  devServer: {
+    static: path.resolve(__dirname, 'dist'),
+    port: 9000,
+    open: true,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      // "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      // "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+    }
+  },
+  module: {
+    rules: [
+        {
+            test: /\.(?:js|mjs|cjs)$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: [
+                  ['@babel/preset-env', { targets: "defaults" }]
+                ]
+              }
+            }
+          },
+        {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+        },
+        {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: 'asset/resource',
+        },
+        {
+            test: /\.(woff|woff2|eot|ttf|otf)$/i,
+            type: 'asset/resource',
+        },
+    ],
+  },
+};
